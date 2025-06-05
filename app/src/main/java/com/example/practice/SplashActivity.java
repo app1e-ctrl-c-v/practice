fb027d8e3d6c862fc5c4285d40e56ba42c643d74
gress <gress_apple@mail.ru>
2025-06-05 21:59:31 +0600

    добавлены ресурсы и создан загрузочный экран

---
+++ app/src/main/java/com/example/practice/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.practice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+
+    }
+}
